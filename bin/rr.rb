f1d1c010408764f44b7f1a7c47f25cfc73ac6c88
Jake Gordon <jake@codeincomplete.com>
2014-08-16 08:45:18 -0700

    allow adapter (bunny vs amqp) to be specified in rr executable

--- bin/rr.rb
+++ bin/rr.rb
@@ -1,20 +1,20 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 #
-# TEMPORARY HACK client script for testing RackRabbit::Client
+# Lightweight executable for using RackRabbit::Client to access a RackRabbit server
 #
-# TODO: extend to support all patterns
+# TODO: extend to support additional patterns
 #
-#  - Req/Response (GET/POST/PUT/HEAD/DELETE)
 #  - Worker (ENQUEUE)
 #  - Pub/Sub (PUBLISH)
 #
 
 require "optparse"
+
 require "rack-rabbit"
 require "rack-rabbit/client"
 
-action  = :run
+action  = :execute
 options = {
   :queue  => "rack-rabbit",
   :method => nil,
@@ -22,22 +22,24 @@ options = {
   :body   => nil
 }
 
-queue_help  = "specify the queue to publish the message (default: rack-rabbit)"
-method_help = "specify the method used to publish the message (default: GET)"
-path_help   = "specify the path of the message (default '')"
-body_help   = "specify the body of the message (default '')"
- 
+queue_help   = "specify the queue to publish the message (default: rack-rabbit)"
+method_help  = "specify the method used to publish the message (default: GET)"
+path_help    = "specify the path of the message (default '')"
+body_help    = "specify the body of the message (default '')"
+adapter_help = "specify an alternate rabbit client gem (default: bunny)"
+
 op = OptionParser.new
 op.banner = "Make requests to rabbitMQ consumer processes"
 op.separator "Usage: rr [options] [METHOD] [PATH] [BODY]"
 op.separator ""
 
-op.on("-h", "--help")                       { action = :help    }
-op.on("-v", "--version")                    { action = :version }
-op.on("-q", "--queue QUEUE",   queue_help)  { |value| options[:queue]  = value }
-op.on("-m", "--method METHOD", method_help) { |value| options[:method] = value }
-op.on("-p", "--path PATH",     path_help)   { |value| options[:path]   = value }
-op.on("-b", "--body BODY",     body_help)   { |value| options[:body]   = value }
+op.on("-h", "--help")                          { action = :help    }
+op.on("-v", "--version")                       { action = :version }
+op.on("-q", "--queue QUEUE",   queue_help)     { |value| options[:queue]   = value                      }
+op.on("-m", "--method METHOD", method_help)    { |value| options[:method]  = value.to_s.upcase.to_sym   }
+op.on("-p", "--path PATH",     path_help)      { |value| options[:path]    = value                      }
+op.on("-b", "--body BODY",     body_help)      { |value| options[:body]    = value                      }
+op.on("-a", "--adapter ADAPTER", adapter_help) { |value| options[:adapter] = value.to_s.downcase.to_sym }
 op.separator ""
 
 op.parse!(ARGV)
@@ -53,13 +55,28 @@ end
 options[:path] ||= ARGV.shift
 options[:body] ||= ARGV.shift
 
-case action
-when :help    then puts op.to_s
-when :version then puts RackRabbit::VERSION
-else
+def execute(options)
   puts RackRabbit::Client.request(options[:queue],
                                   options[:method],
                                   options[:path],
                                   options[:body])
 end
 
+case action
+when :help    then puts op.to_s
+when :version then puts RackRabbit::VERSION
+else
+
+  if options[:adapter] == :amqp        # then need a running EventMachine reactor...
+    require 'eventmachine'
+    EventMachine.run do
+      EventMachine.defer do   # ... but don't block the reactor
+        execute(options)
+        EventMachine.stop
+      end
+    end
+  else                        # otherwise just run directly on main thread
+    execute(options)
+  end
+
+end
