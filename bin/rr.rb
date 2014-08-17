ceb9cad5e8c9b2630c3bdd7b8dc6d74741456db0
Jake Gordon <jake@codeincomplete.com>
2014-08-17 08:54:54 -0700

    provide --include (ruby option) to both executables

--- bin/rr.rb
+++ bin/rr.rb
@@ -1,18 +1,14 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 #
-# Lightweight executable for using RackRabbit::Client to access a RackRabbit server
+# Make requests to a RackRabbit server.
 #
-# TODO: extend to support additional patterns
-#
-#  - Worker (ENQUEUE)
-#  - Pub/Sub (PUBLISH)
-#
+
+#==============================================================================
+# PARSE COMMAND LINE OPTIONS
+#==============================================================================
 
 require "optparse"
-
-require "rack-rabbit"
-require "rack-rabbit/client"
 
 action  = :execute
 options = {
@@ -22,61 +18,84 @@ options = {
   :body   => nil
 }
 
+summary      = "Make requests to rabbitMQ consumer processes hosted by a RackRabbit server."
+usage        = "Usage:  rr [options] [METHOD] [PATH] [BODY]"
+banner       = "#{summary}\n\n#{usage}"
 queue_help   = "specify the queue to publish the message (default: rack-rabbit)"
 method_help  = "specify the method used to publish the message (default: GET)"
 path_help    = "specify the path of the message (default '')"
 body_help    = "specify the body of the message (default '')"
 adapter_help = "specify an alternate rabbit client gem (default: bunny)"
+include_help = "specify an additional $LOAD_PATH (may be used more than once)"
 
 op = OptionParser.new
-op.banner = "Make requests to rabbitMQ consumer processes"
-op.separator "Usage: rr [options] [METHOD] [PATH] [BODY]"
-op.separator ""
+op.banner = banner
 
-op.on("-h", "--help")                          { action = :help    }
-op.on("-v", "--version")                       { action = :version }
-op.on("-q", "--queue QUEUE",   queue_help)     { |value| options[:queue]   = value                      }
-op.on("-m", "--method METHOD", method_help)    { |value| options[:method]  = value.to_s.upcase.to_sym   }
-op.on("-p", "--path PATH",     path_help)      { |value| options[:path]    = value                      }
-op.on("-b", "--body BODY",     body_help)      { |value| options[:body]    = value                      }
+op.separator ""
+op.separator "RackRabbit options:"
+op.on("-q", "--queue QUEUE",     queue_help)   { |value| options[:queue]   = value                      }
+op.on("-m", "--method METHOD",   method_help)  { |value| options[:method]  = value.to_s.upcase.to_sym   }
+op.on("-p", "--path PATH",       path_help)    { |value| options[:path]    = value                      }
+op.on("-b", "--body BODY",       body_help)    { |value| options[:body]    = value                      }
 op.on("-a", "--adapter ADAPTER", adapter_help) { |value| options[:adapter] = value.to_s.downcase.to_sym }
-op.separator ""
 
+op.separator ""
+op.separator "Ruby options:"
+op.on("-I", "--include PATH", include_help) { |value| $LOAD_PATH.unshift(*value.split(":")) }
+
+op.separator ""
+op.separator "Common options:"
+op.on("-h", "--help")    { action = :help    }
+op.on("-v", "--version") { action = :version }
+
+op.separator ""
 op.parse!(ARGV)
 
 if options[:method].nil?
-  if options[:method] = RackRabbit.parse_method(ARGV[0], :default => nil)
+  if options[:method] = ["GET", "POST", "PUT", "DELETE"].find{|m| m == ARGV[0].to_s.upcase }
     ARGV.delete_at(0)
   else
-    options[:method] = "GET"
+    options[:method] = :GET
   end
 end
 
 options[:path] ||= ARGV.shift
 options[:body] ||= ARGV.shift
 
-def execute(options)
-  puts RackRabbit::Client.request(options[:queue],
-                                  options[:method],
-                                  options[:path],
-                                  options[:body])
-end
+#==============================================================================
+# EXECUTE script (within an EM reactor if necessary)
+#==============================================================================
+
+require 'rack-rabbit'
+require 'rack-rabbit/client'
+
+module RR
+
+  def self.make_request(options)
+    puts RackRabbit::Client.request(options[:queue], options[:method], options[:path], options[:body])
+  end
+
+  def self.execute(options)
+    if options[:adapter] == :amqp  # then need a running EventMachine reactor...
+      require 'eventmachine'
+      EventMachine.run do
+        EventMachine.defer do      # ... but don't block the reactor
+          make_request(options)
+          EventMachine.stop
+        end
+      end
+    else                           # otherwise just run directly on main thread
+      make_request(options)
+    end
+  end
+
+end # module RR
 
 case action
 when :help    then puts op.to_s
 when :version then puts RackRabbit::VERSION
 else
-
-  if options[:adapter] == :amqp        # then need a running EventMachine reactor...
-    require 'eventmachine'
-    EventMachine.run do
-      EventMachine.defer do   # ... but don't block the reactor
-        execute(options)
-        EventMachine.stop
-      end
-    end
-  else                        # otherwise just run directly on main thread
-    execute(options)
-  end
-
+  RR.execute(options)
 end
+
+#==============================================================================
--- lib/rack-rabbit.rb
+++ lib/rack-rabbit.rb
@@ -8,7 +8,6 @@ module RackRabbit
 
   VERSION = "0.0.1"
   SUMMARY = "A Unicorn-style preforking, rack-based server for hosting RabbitMQ consumer processes"
-  METHODS = [ :GET, :POST, :PUT, :DELETE ] # ... and coming soon - ENQUEUE and PUBLISH
 
   #----------------------------------------------------------------------------
 
@@ -47,19 +46,6 @@ module RackRabbit
     when :TERM then "TERMINATE"
     else
       sig
-    end
-  end
-
-  #----------------------------------------------------------------------------
-
-  def self.parse_method(method, options = {})
-    method = method.to_s.upcase.to_sym
-    if METHODS.include?(method)
-      method
-    elsif options.key?(:default)
-      options[:default]
-    else
-      raise ArgumentError, "unknown method #{method} - must be one of #{METHODS.join(', ')}"
     end
   end
 
